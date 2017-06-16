ca17832efe2e7ef1b4490f25bbc8a6a5da3c31ce
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-06-16 22:51:32 +0200

    script creation database

---
+++ database/create_db.sql
@@ -0,0 +1,75 @@
+CREATE TABLE players(
+   id INTEGER PRIMARY KEY AUTOINCREMENT, 
+   name TEXT,
+   prenom TEXT,
+   stableford INTEGER, --index in french
+   sex INTEGER -- 0 female 1 male 
+);
+
+CREATE TABLE game(
+	id INTEGER PRIMARY KEY AUTOINCREMENT, 
+	id_player INTEGER 
+	date TEXT, 
+	golf TEXT, 	
+	score_brut INTEGER, -- (total of ) for the game
+	score_net INTEGER, --(total of )  for the game
+	stableford INTEGER, -- (only for 18 holes)
+	FOREIGN KEY(id_player) REFERENCES players(id),
+	FOREIGN KEY(golf) REFERENCES golf(name)
+);
+
+CREATE TABLE hole(
+   id INTEGER PRIMARY KEY AUTOINCREMENT, 
+   id_player INTEGER, -- maybe useless can be find with game
+   golf TEXT, -- maybe useless can be find with game
+   hole_number INTEGER, -- in order to have handicap and par
+   bunker INTEGER, -- 0 yes 1 no
+   fairway INTEGER, -- 0 yes 1 no 2 green (green only for par 3)
+   nb_puts INTEGER, 
+   score_brute INTEGER, -- number of (coup joué ) for the hole
+   score_net INTEGER, -- number of (coup au dessus du par) for the hole
+   FOREIGN KEY(id_player) REFERENCES players(id)
+   FOREIGN KEY(golf) REFERENCES golf(name)
+);
+
+CREATE TABLE golf(
+   name TEXT PRIMARY KEY, 
+   slope INTEGER,
+   sss INTEGER, 
+   handicap_hole_1 INTEGER,
+   handicap_hole_2 INTEGER,
+   handicap_hole_3 INTEGER, 
+   handicap_hole_4 INTEGER,
+   handicap_hole_5 INTEGER,
+   handicap_hole_6 INTEGER,
+   handicap_hole_7 INTEGER,
+   handicap_hole_8 INTEGER,
+   handicap_hole_9 INTEGER,
+   handicap_hole_10 INTEGER,
+   handicap_hole_11 INTEGER,
+   handicap_hole_12 INTEGER,
+   handicap_hole_13 INTEGER,
+   handicap_hole_14 INTEGER,
+   handicap_hole_15 INTEGER,
+   handicap_hole_16 INTEGER,
+   handicap_hole_17 INTEGER,
+   handicap_hole_18 INTEGER, 
+   par_hole_1 INTEGER,
+   par_hole_2 INTEGER,
+   par_hole_3 INTEGER, 
+   par_hole_4 INTEGER,
+   par_hole_5 INTEGER,
+   par_hole_6 INTEGER,
+   par_hole_7 INTEGER,
+   par_hole_8 INTEGER,
+   par_hole_9 INTEGER,
+   par_hole_10 INTEGER,
+   par_hole_11 INTEGER,
+   par_hole_12 INTEGER,
+   par_hole_13 INTEGER,
+   par_hole_14 INTEGER,
+   par_hole_15 INTEGER,
+   par_hole_16 INTEGER,
+   par_hole_17 INTEGER,
+   par_hole_18 INTEGER, 
+);
