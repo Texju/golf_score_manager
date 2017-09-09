5d2f94d1c0a3b6bef9eea6c9e9648ffb0aa16a1d
Julien Couillard <julien.couillard@etudiant.univ-rennes1.fr>
2017-09-09 16:28:33 +0200

    add golfs

--- app.py
+++ app.py
@@ -41,14 +41,14 @@ def index():
 @app.route('/players')
 def players():
     db = get_db()
-    cur = db.execute('select firstname, lastname, stableford from database order by id asc')
+    cur = db.execute('select firstname, lastname, stableford from players order by id asc')
     entries = [dict(firstname=row[0], lastname=row[1], stableford=row[1]) for row in cur.fetchall()]
     return jsonify(tasks=entries)
 
 @app.route('/players/new', methods=['POST'])
 def new_player():
     db = get_db()
-    cur = db.execute('insert into database (firstname,lastname,stableford,sex) values (?, ?, ?, ?)',
+    cur = db.execute('insert into players (firstname,lastname,stableford,sex) values (?, ?, ?, ?)',
                [request.json['firstname'], request.json['lastname'], request.json['stableford'], request.json['sex']])
     db.commit()
     id = cur.lastrowid
@@ -61,22 +61,58 @@ def new_player():
 @app.route('/golfs')
 def golfs():
     db = get_db()
-    cur = db.execute('select * from database order by id asc')
+    cur = db.execute('select * from golfs order by id asc')
     entries = [dict(firstname=row[0], lastname=row[1], stableford=row[1]) for row in cur.fetchall()]
     return jsonify(tasks=entries)
 
 @app.route('/golfs/new', methods=['POST'])
 def new_golf():
     db = get_db()
-    cur = db.execute('insert into database (firstname,lastname,stableford,sex) values (?, ?, ?, ?)',
-               [request.json['firstname'], request.json['lastname'], request.json['stableford'], request.json['sex']])
+    cur = db.execute('insert into golfs (name,slope,sss,handicap_hole_1,handicap_hole_2,handicap_hole_3,handicap_hole_4,handicap_hole_5,handicap_hole_6,handicap_hole_7,handicap_hole_8,handicap_hole_9,handicap_hole_10,handicap_hole_11,handicap_hole_12,handicap_hole_13,handicap_hole_14,handicap_hole_15,handicap_hole_16,handicap_hole_17,handicap_hole_18,par_hole_1,par_hole_2,par_hole_3,par_hole_4,par_hole_5,par_hole_6,par_hole_7,par_hole_8,par_hole_9,par_hole_10,par_hole_11,par_hole_12,par_hole_13,par_hole_14,par_hole_15,par_hole_16,par_hole_17,par_hole_18) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+               [request.json['name'],request.json['slope'],request.json['sss'],request.json['handicap_hole_1'],request.json['handicap_hole_2'],request.json['handicap_hole_3'],request.json['handicap_hole_4'],request.json['handicap_hole_5'],request.json['handicap_hole_6'],request.json['handicap_hole_7'],request.json['handicap_hole_8'],request.json['handicap_hole_9'],request.json['handicap_hole_10'],request.json['handicap_hole_11'],request.json['handicap_hole_12'],request.json['handicap_hole_13'],request.json['handicap_hole_14'],request.json['handicap_hole_15'],request.json['handicap_hole_16'],request.json['handicap_hole_17'],request.json['handicap_hole_18'],request.json['par_hole_1'],request.json['par_hole_2'],request.json['par_hole_3'],request.json['par_hole_4'],request.json['par_hole_5'],request.json['par_hole_6'],request.json['par_hole_7'],request.json['par_hole_8'],request.json['par_hole_9'],request.json['par_hole_10'],request.json['par_hole_11'],request.json['par_hole_12'],request.json['par_hole_13'],request.json['par_hole_14'],request.json['par_hole_15'],request.json['par_hole_16'],request.json['par_hole_17'],request.json['par_hole_18']])
     db.commit()
     id = cur.lastrowid
-    return jsonify({"firstname": request.json['firstname'],
-                    "lastname": request.json['lastname'],
-                    "stableford": request.json['stableford'],
-                    "sex": request.json['sex'],
-                    "id": id})
+    return jsonify({
+        "name" : request.json['name'],
+        "slope" : request.json['slope'],
+        "sss" : request.json['sss'],
+        "handicap_hole_1" : request.json['handicap_hole_1'],
+        "handicap_hole_2" : request.json['handicap_hole_2'],
+        "handicap_hole_3" : request.json['handicap_hole_3'],
+        "handicap_hole_4" : request.json['handicap_hole_4'],
+        "handicap_hole_5" : request.json['handicap_hole_5'],
+        "handicap_hole_6" : request.json['handicap_hole_6'],
+        "handicap_hole_7" : request.json['handicap_hole_7'],
+        "handicap_hole_8" : request.json['handicap_hole_8'],
+        "handicap_hole_9" : request.json['handicap_hole_9'],
+        "handicap_hole_10" : request.json['handicap_hole_10'],
+        "handicap_hole_11" : request.json['handicap_hole_11'],
+        "handicap_hole_12" : request.json['handicap_hole_12'],
+        "handicap_hole_13" : request.json['handicap_hole_13'],
+        "handicap_hole_14" : request.json['handicap_hole_14'],
+        "handicap_hole_15" : request.json['handicap_hole_15'],
+        "handicap_hole_16" : request.json['handicap_hole_16'],
+        "handicap_hole_17" : request.json['handicap_hole_17'],
+        "handicap_hole_18" : request.json['handicap_hole_18'],
+        "par_hole_1" : request.json['par_hole_1'],
+        "par_hole_2" : request.json['par_hole_2'],
+        "par_hole_3" : request.json['par_hole_3'],
+        "par_hole_4" : request.json['par_hole_4'],
+        "par_hole_5" : request.json['par_hole_5'],
+        "par_hole_6" : request.json['par_hole_6'],
+        "par_hole_7" : request.json['par_hole_7'],
+        "par_hole_8" : request.json['par_hole_8'],
+        "par_hole_9" : request.json['par_hole_9'],
+        "par_hole_10" : request.json['par_hole_10'],
+        "par_hole_11" : request.json['par_hole_11'],
+        "par_hole_12" : request.json['par_hole_12'],
+        "par_hole_13" : request.json['par_hole_13'],
+        "par_hole_14" : request.json['par_hole_14'],
+        "par_hole_15" : request.json['par_hole_15'],
+        "par_hole_16" : request.json['par_hole_16'],
+        "par_hole_17" : request.json['par_hole_17'],
+        "par_hole_18" : request.json['par_hole_18']})
+
 
 if __name__ == '__main__':
     if os.path.exists(DATABASE):
